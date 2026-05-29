9d15d96399d69e29b23bdccc3450603cab1ad2df
c3479565 <c3479565@uon.edu.au>
2026-05-29 20:12:04 +1000

    Added support for options.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,71 @@ use std::time::Duration;
 use shakmaty::{Chess, Position, FromSetup, fen::Fen, uci::UciMove, Color, CastlingMode, EnPassantMode};
 use shakmaty::zobrist::Zobrist64;
 use hodgey_chess_engine::find_best_move_with_time;
-use uci_parser::{UciCommand, UciResponse, UciOption, UciOptionType};
+use uci_parser::{UciCommand, UciOption, UciResponse};
 
-const MOVE_OVERHEAD: Duration = Duration::from_millis(100);
-const MAX_MOVE_TIME: Duration = Duration::from_secs(10);
-//Currently doesn't actually support any options, just pretends it does
-const OPTIONS: [(&str, UciOptionType); 1] = [
-    ("move overhead", UciOptionType::Spin { default: 100, min: 0, max: 1000 })
+const OPTIONS: [HodgeyUciOption; 3] = [
+    HodgeyUciOption::new("MoveOverhead", 100, 0, 1000),
+    HodgeyUciOption::new("MaxMoveTime", 10_000, 0, 60_000),
+    HodgeyUciOption::new("MaxPercentageOfTimePerMove", 10, 1, 10)
 ];
 
-#[derive(std::default::Default)]
+struct HodgeyUciOption {
+    name: &'static str,
+    default: i32,
+    min: i32,
+    max: i32,
+    value: i32
+}
+
+impl HodgeyUciOption {
+    const fn new(name: &'static str, default: i32, min: i32, max: i32) -> HodgeyUciOption {
+        HodgeyUciOption { name, default, min, max, value: default }
+    }
+    
+    fn to_uci_option(&self) -> UciOption {
+        UciOption::spin(self.name, self.default, self.min, self.max)
+    }
+}
+
 struct GameState {
     chess: Chess,
     previously_seen_hashes: Vec<u64>,
     debug_enabled: bool,
+    options: [HodgeyUciOption; 3]
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        GameState {
+            options: OPTIONS,
+            chess: Default::default(),
+            previously_seen_hashes: Default::default(),
+            debug_enabled: Default::default()
+        }
+    }
+}
+
+impl GameState {
+    /// Exepcts that name will be a valid option name
+    fn option_value_unchecked(&self, name: &str) -> i32 {
+        let option = self.options.iter().find(|opt| opt.name == name);
+        //Expect valid option to be passed
+        option.unwrap().value
+    }
+
+    fn max_move_time(&self) -> Duration {
+        let value = self.option_value_unchecked("MaxMoveTime");
+        Duration::from_millis(value as u64)
+    }
+
+    fn move_overhead(&self) -> Duration {
+        let value = self.option_value_unchecked("MoveOverhead");
+        Duration::from_millis(value as u64)
+    }
+
+    fn percentage_of_time_per_move(&self) -> i32 {
+        self.option_value_unchecked("MaxPercentageOfTimePerMove")
+    }
 }
 
 fn main() {
@@ -40,8 +91,8 @@ fn main() {
             UciCommand::IsReady => println!("{}", UciResponse::readyok()), 
             UciCommand::Position { fen, moves } => update_position(fen, moves, &mut game_state),
             UciCommand::Go(search_options) => print_best_move(&search_options, &mut game_state),
+            UciCommand::SetOption { name, value } => set_option(name, value, &mut game_state),
             UciCommand::Quit => break,
-            // TODO make options work in some form
             //SetOption, Register, UciNewGame, Stop, and PonderHit are all currently ignored
             _ => {}
         }
@@ -56,8 +107,26 @@ fn print_uci_response() {
 }
 
 fn display_options() {
-    for (name, option_type) in OPTIONS {
-        println!("{}", UciResponse::Option::<String>(UciOption::new(name, option_type)));
+    for opt in OPTIONS {
+        println!("{}", UciResponse::Option(opt.to_uci_option()));
+    }
+}
+
+fn set_option(name: String, value: Option<String>, game_state: &mut GameState) {
+    let option = game_state.options.iter_mut().find(|opt| opt.name == name);
+    let Some(option) = option else {
+        //return if option doesn't exist
+        return;
+    };
+    let value = value.and_then(|v| v.parse::<i32>().ok());
+    
+    let Some(value) = value else {
+        //return if no value given
+        return;
+    };
+
+    if value >= option.min && value <= option.max {
+        option.value = value;
     }
 }
 
@@ -66,11 +135,21 @@ fn print_best_move(options: &uci_parser::UciSearchOptions, game_state: &mut Game
         Color::White => options.wtime,
         Color::Black => options.btime,
     }
-        .map(|d| (d / 8).min(MAX_MOVE_TIME))
-        .unwrap_or(MAX_MOVE_TIME);
+        //use only the correct percentage of remaining time
+        .map(|d| d * 100 / game_state.percentage_of_time_per_move() as u32)
+        //limit move time to max move time
+        .map(|d| d.min(game_state.max_move_time()));
 
-    let mut search_time = options.movetime.unwrap_or(search_time);
-    search_time -= MOVE_OVERHEAD; //Add move overhead or something (I have no clue what I am doing)
+    let mut search_time = options.movetime
+        .or(search_time)
+        .unwrap_or(game_state.max_move_time());
+    
+    if search_time < game_state.move_overhead() {
+        search_time = Duration::ZERO;
+    }
+    else {
+        search_time -= game_state.move_overhead();
+    }
 
     let best_move = find_best_move_with_time(&game_state.chess, search_time, &mut game_state.previously_seen_hashes);
     println!("bestmove {}", best_move.to_uci(CastlingMode::Standard));
@@ -89,6 +168,7 @@ fn update_position(fen: Option<String>, moves: Vec<String>, game_state: &mut Gam
         let Some(legal_move) = m.parse::<UciMove>().ok()
             .and_then(|m| m.to_move(&chess).ok())
         else {
+            //Ignores moves it can't understand
             continue;
         };
 
